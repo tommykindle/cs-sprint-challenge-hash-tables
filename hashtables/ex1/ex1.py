23b3fb94ef56e011ea07962173db5a28e4fe1098
Tommy <tommykindle83@gmail.com>
2020-09-14 17:27:15 -0700

    finished with ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,12 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = dict()
+    for i in range(length):
+        cache[weights[i]] = i
+    for i in range(length):
+        items = limit - weights[i]
+        if items in cache:
+            return(cache[items], weights.index(weights[i]))
 
     return None
